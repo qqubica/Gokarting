3c4d99ae9fd2f155ea70347fbb90db26c39d1c31
Kuba Wudarski <kuba@wudarscy.pl>
2023-06-12 22:16:22 +0200

    initiate gokarts with localization id

--- src/main/java/com/example/backend/service/GokartService.java
+++ src/main/java/com/example/backend/service/GokartService.java
@@ -1,11 +1,13 @@
 package com.example.backend.service;
 
 import com.example.backend.model.Gokart;
+import com.example.backend.model.Localisation;
 import com.example.backend.repository.GokartRepository;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Random;
 
 @Service
@@ -13,20 +15,23 @@ import java.util.Random;
 public class GokartService {
     GokartRepository gokartRepository;
     @Autowired
-    public GokartService(GokartRepository gokartRepository) {
+    public GokartService(GokartRepository gokartRepository, LocalisationService localisationService) {
         this.gokartRepository = gokartRepository;
         if (gokartRepository.count() == 0) {
-            populateWithRandomData(10);
+            populateWithRandomData(10, localisationService);
             System.out.println("Populated gokart repository with random data.");
         }
     }
-    public void populateWithRandomData(int n) {
+    public void populateWithRandomData(int n, LocalisationService localisationService) {
         Random random = new Random();
+        List<Localisation> localisations = localisationService.getLocalisationsList();
         for (int i = 0; i < n; i++) {
             Gokart gokart = new Gokart();
 
             gokart.setHorsepower(random.nextInt(100));
             gokart.setTypGokarta(Gokart.GokartType.values()[random.nextInt(Gokart.GokartType.values().length)]);
+            Localisation randomLoc = localisations.get(random.nextInt(localisations.size()));
+            gokart.setLocalisation(randomLoc);
 
             gokartRepository.save(gokart);
         }
