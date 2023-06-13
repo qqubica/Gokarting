01e2400beca59a49f4037fb6639e90b3de032d1c
Kuba Wudarski <kuba@wudarscy.pl>
2023-06-13 14:58:53 +0200

    code clean up

--- src/main/java/com/example/backend/controller/RideController.java
+++ src/main/java/com/example/backend/controller/RideController.java
@@ -3,6 +3,7 @@ package com.example.backend.controller;
 import com.example.backend.model.DTO.RidePrepare;
 import com.example.backend.service.RideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/backend/service/RideGokartClientService.java
+++ src/main/java/com/example/backend/service/RideGokartClientService.java
@@ -1,39 +1,41 @@
 package com.example.backend.service;
 
 import com.example.backend.model.Client;
+import com.example.backend.model.Ride;
 import com.example.backend.model.RideGokartClient;
 import com.example.backend.repository.RideGokartClientRepository;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Service
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class RideGokartClientService {
     RideGokartClientRepository rideGokartClientRepository;
     @Autowired
-    public RideGokartClientService(RideGokartClientRepository rideGokartClientRepository, ClientService clientService) {
+    public RideGokartClientService(RideGokartClientRepository rideGokartClientRepository, ClientService clientService, RideService rideService) {
         this.rideGokartClientRepository = rideGokartClientRepository;
         if (rideGokartClientRepository.count() == 0) {
-            addRandomData(5, clientService);
+            addRandomData(30, clientService, rideService);
             System.out.println("Populated rideGokartClient repository with random data.");
         }
     }
 
-    public void addRandomData(int n, ClientService clientService){
+    public void addRandomData(int n, ClientService clientService, RideService rideService) {
         Random random = new Random();
         List<Client> clients =  clientService.getClientsList();
+        List<Ride> rides = rideService.getRidesList();
+
         for (int i = 0; i < n; i++) {
             RideGokartClient rideGokartClient = new RideGokartClient();
 
             rideGokartClient.setClient(clients.get(random.nextInt(clients.size())));
 
+            rideGokartClient.setRide(rides.get(random.nextInt(rides.size())));
+
             rideGokartClientRepository.save(rideGokartClient);
         }
     }
-
-
 }
