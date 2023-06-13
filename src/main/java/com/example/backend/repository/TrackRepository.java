7430f9d702fc0d778f322d7eeec830190d01c53f
Kuba Wudarski <kuba@wudarscy.pl>
2023-06-13 14:59:29 +0200

    code clean up

--- src/main/java/com/example/backend/repository/TrackRepository.java
+++ src/main/java/com/example/backend/repository/TrackRepository.java
@@ -2,9 +2,7 @@ package com.example.backend.repository;
 
 import com.example.backend.model.Localisation;
 import com.example.backend.model.Track;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.data.repository.Repository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
 
