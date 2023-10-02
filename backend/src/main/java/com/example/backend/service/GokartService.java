eb91fc35d2da8d8ae3bc2d4e59c24d591553d5e7
Kuba Wudarski <kuba@wudarscy.pl>
2023-10-02 21:54:51 +0200

    clean up

--- backend/src/main/java/com/example/backend/service/GokartService.java
+++ backend/src/main/java/com/example/backend/service/GokartService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
